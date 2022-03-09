c38a6ff6bb4cd52caafa0e65c4b2bc24e35fa6b2
Bruno Netto <brunonettomac@outlook.com>
2022-03-08 22:36:34 -0300

    Adicionando componente base de react modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+// É uma questão de acessibilidade, para falar qual é o elemento root da nossa aplicação para rodar o modal
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />   
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />   
       <Dashboard />  
       <GlobalStyle />
+
+      <Modal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 }

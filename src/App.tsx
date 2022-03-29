b1154f56a0a32ce2810085da15ab9e70380ecd31
Bruno Netto <brunonettomac@outlook.com>
2022-03-28 21:24:01 -0300

    Adicionando Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext } from './TransactionsContext';
 
 // É uma questão de acessibilidade, para falar qual é o elemento root da nossa aplicação para rodar o modal
 Modal.setAppElement('#root');
@@ -20,11 +21,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />   
       <Dashboard />  
       <GlobalStyle />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
-    </>
+    </TransactionsContext.Provider>
   );
 }
